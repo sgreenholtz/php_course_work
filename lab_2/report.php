4f58d83e069a936ca8d3176f274c1f1c1a417418
Sebastian <smgreenholtz@gmail.com>
2015-09-04 01:38:57 +0000

    Coding up PHP

---
+++ lab_2/report.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+        <title>Aliens Abducted Me - Report an Abduction</title>
+    </head>
+    
+    <body>
+        <h2>Aliens Abducted Me - Report an Abduction</h2>
+    
+    <?php 
+    
+    
+    
+    ?>
+    
+    </body>
+</html>
