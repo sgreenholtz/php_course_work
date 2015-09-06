5af4f9fdb24b8232fe8268353900a62291317683
Sebastian <smgreenholtz@gmail.com>
2015-09-06 18:16:15 +0000

    Done!

--- lab_2/report.php
+++ lab_2/report.php
@@ -8,15 +8,36 @@
     
     <?php 
     
+        $name = $_POST['firstname'] . ' ' . $_POST['lastname'];
+        
         $when_it_happened = $_POST['whenithappened'];
         
         $how_long = $_POST['howlong'];
         
+        $how_many = $_POST['howmany'];
+        
         $alien_description = $_POST['aliendescription'];
+        
+        $what_they_did = $_POST['whattheydid'];
     
         $fang_spotted = $_POST['fangspotted'];
         
+        $other = $_POST['other'];
+        
         $email = $_POST['email'];
+        
+        $msg = "$name was adbudcted $when_it_happened and was gone for $how_long.\n" .
+            "Number of aliens: $how_many\n" .
+            "Alien description: $alien_description\n" .
+            "What they did: $what_they_did\n" .
+            "Fang spotted: $fang_spotted\n" .
+            "Other comments: $other";
+            
+        $to = 'owen@aliensabductedme.com';
+        
+        $subject = 'Aliens Abducted Me - Abduction Report';
+        
+        mail($to, $subject, $msg, 'From:' . $email);
         
         echo 'Thanks for submitting the form.<br />';
         
@@ -24,9 +45,16 @@
         
         echo ' and were gone for ' . $how_long . '<br />';
         
+        echo 'Number of Aliens: ' . $how_many . 'br />';
+        
+        echo 'Describe them: ' . $alien_description . 'br />';
+        
         echo 'Was Fang there? ' . $fang_spotted . '<br />';
         
+        echo 'Other comments: ' . $other . '<br />';
+        
         echo 'Your email address is ' . $email;
+    
     ?>
     
     </body>
