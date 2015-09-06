28af6dceffd8a79c455c27314cb18004aa366c82
Sebastian <smgreenholtz@gmail.com>
2015-09-06 20:06:40 +0000

    SQL updates

--- lab_2/lab2.sql
+++ lab_2/lab2.sql
@@ -11,4 +11,10 @@ CREATE TABLE aliens_abduction {
     fang_spotted        varchar(10),
     other               varchar(100),
     email               varchar(50),
-    };
+    }
+    
+INSERT INTO aliens_abduction (first_name, last_name, when_it_happened, how_long,
+    how_many, alien_description, what_they_did, fang_spotted, other, email)
+VALUES ('Sally', 'Jones', '3 days ago', '1 day', 'four', 'green with six tentacles',
+        'We just talked and played with a dog', 'yes',
+        'I may have seen your dog. Contact me.', 'sally@gregs-list.net')
