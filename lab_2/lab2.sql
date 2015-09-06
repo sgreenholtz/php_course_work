7af3e4c0058a5f9ece57b11428172502413a5189
Sebastian <smgreenholtz@gmail.com>
2015-09-06 20:34:45 +0000

    Lab 2 updates to PHP for MySQL

--- lab_2/lab2.sql
+++ lab_2/lab2.sql
@@ -1,6 +1,4 @@
-CREATE DATABASE aliendatabase;
-USE aliendatabase;
-CREATE TABLE aliens_abduction {
+CREATE TABLE aliens_abduction (
     first_name          varchar(30),
     last_name           varchar(30),
     when_it_happened    varchar(30),
@@ -10,11 +8,18 @@ CREATE TABLE aliens_abduction {
     what_they_did       varchar(100),
     fang_spotted        varchar(10),
     other               varchar(100),
-    email               varchar(50),
-    }
+    email               varchar(50)
+    );
     
 INSERT INTO aliens_abduction (first_name, last_name, when_it_happened, how_long,
-    how_many, alien_description, what_they_did, fang_spotted, other, email)
+    how_many, alien_description, what_they_did, fang_spotted, other, email);
 VALUES ('Sally', 'Jones', '3 days ago', '1 day', 'four', 'green with six tentacles',
         'We just talked and played with a dog', 'yes',
-        'I may have seen your dog. Contact me.', 'sally@gregs-list.net')
+        'I may have seen your dog. Contact me.', 'sally@gregs-list.net');
+        
+SELECT first_name, last_name
+FROM aliens_abduction;
+        
+SELECT *
+FROM aliens_abduction;
+
--- lab_2/report.php
+++ lab_2/report.php
@@ -36,8 +36,20 @@
         $to = 'owen@aliensabductedme.com';
         
         $subject = 'Aliens Abducted Me - Abduction Report';
+    
+        $dbc = mysqli_connect('localhost', 'sgreenholtz', '', 'c9')
+            or die ('Error connecting to MySQL server.');
         
-        mail($to, $subject, $msg, 'From:' . $email);
+        $query = "INSERT INTO aliens_abduction (first_name, last_name, when_it_happened, how_long," .
+            "how_many, alien_description, what_they_did, fang_spotted, other, email)" .
+            "VALUES ('Sally', 'Jones', '3 days ago', '1 day', 'four', 'green with six tentacles'," .
+            "'We just talked and played with a dog', 'yes', 'I may have seen your dog. Contact me.'," .
+            "'sally@gregs-list.net');";
+        
+        $result = mysqli_query($dbc, $query)
+            or die('Error querying database.');
+        
+        mysqli_close($dbc);
         
         echo 'Thanks for submitting the form.<br />';
         
