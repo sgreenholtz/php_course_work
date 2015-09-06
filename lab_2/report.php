c9fb80d50e12c29f2cddeeebc7c266c682538f99
Sebastian <smgreenholtz@gmail.com>
2015-09-06 17:59:20 +0000

    Finished entering PHP

--- lab_2/report.php
+++ lab_2/report.php
@@ -8,8 +8,25 @@
     
     <?php 
     
+        $when_it_happened = $_POST['whenithappened'];
+        
+        $how_long = $_POST['howlong'];
+        
+        $alien_description = $_POST['aliendescription'];
     
-    
+        $fang_spotted = $_POST['fangspotted'];
+        
+        $email = $_POST['email'];
+        
+        echo 'Thanks for submitting the form.<br />';
+        
+        echo 'You were abducted ' . $when_it_happened;
+        
+        echo ' and were gone for ' . $how_long . '<br />';
+        
+        echo 'Was Fang there? ' . $fang_spotted . '<br />';
+        
+        echo 'Your email address is ' . $email;
     ?>
     
     </body>
