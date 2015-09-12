1b1ed743bf13c902adb0628dfd98708213e7096d
Sebastian <smgreenholtz@gmail.com>
2015-09-12 21:27:07 +0000

    Working on Chapter 3

---
+++ lab_3/lab_3.sql
@@ -0,0 +1,22 @@
+CREATE DATABASE elvis_store;
+USE elvis_store;
+CREATE TABLE email_list
+(
+first_name VARCHAR( 20 ) ,
+last_name VARCHAR( 20 ) ,
+email VARCHAR( 60 )
+);
+/*
+  <form method="post" action="addemail.php">
+    <label for="firstname">First name:</label>
+    <input type="text" id="firstname" name="firstname" /><br />
+    <label for="lastname">Last name:</label>
+    <input type="text" id="lastname" name="lastname" /><br />
+    <label for="email">Email:</label>
+    <input type="text" id="email" name="email" /><br />
+    <input type="submit" name="Submit" value="Submit" />
+  </form>
+  */
+  
+<?
+    
