3707078861ffabc7c9767ee70b7a9eb7e99e058d
Sebastian <smgreenholtz@gmail.com>
2015-09-12 21:35:36 +0000

    Update

--- lab_3/lab_3.php
+++ lab_3/lab_3.php
@@ -1,13 +1,13 @@
 <?php
 
+    $dbc = mysqli_connect('localhost', 'sgreenholtz', '', 'c9')
+        or die ('Error connecting to MySQL server.');
+
     $first_name = $_POST("firstname");
     
     $last_name = $_POST("lastname");
     
     $email = $_POST("email");
-
-    $dbc = mysqli_connect('localhost', 'sgreenholtz', '', 'c9')
-        or die ('Error connecting to MySQL server.');
         
     $query = "INSERT INTO email_list (first_name, last_name, email)" .
         "VALUES ($first_name, $last_name, $email)";
@@ -15,5 +15,8 @@
     mysqli_query($dbc, $query)
         or die("Failed to update the database");
         
+    echo 'Customer added';
+    
     mysqli_close($dbc);
+    
 ?>
