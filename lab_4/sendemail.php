976be521bd3a386a962d97f9f066646855e369b6
Sebastian <smgreenholtz@gmail.com>
2015-09-22 22:31:43 +0000

    Created Send Email pages for adding validation

---
+++ lab_4/sendemail.php
@@ -0,0 +1,34 @@
+<?php
+    
+    $subject = $_POST['subject'];
+    
+    $email_body = $_POST['elvismail'];
+    
+    $from = 'sgreenholtz@madisoncollege.edu';
+    
+    if ($empty($email_body) || $empty($subject)) {
+        echo "Subject or Body is empty. Please try again";
+    }
+    else {
+        $dbc = mysqli_connect('localhost', 'sgreenholtz', '', 'elvis_store')
+        or die('Failed to connect to database.');
+    
+        $query = "SELECT * FROM email_list";
+    
+        $result = mysqli_query($dbc, $query);
+        
+        while ($row = mysqli_fetch_array($result)) {
+            $first_name = $row['first_name'];
+            $last_name = $row['last_name'];
+            $email = $row['email'];
+        
+            $msg = "Dear $first_name $last_name, \n$email_body";
+        
+            mail($email, $subject, $msg, 'From:' . $from);
+        
+            echo 'Email sent to: ' . $first_name . ' ' . $last_name . '<br />';
+        }
+    }
+    mysqli_close($dbc);
+
+?>
