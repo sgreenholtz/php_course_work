c8d70a6895591f7247a006a5a6e76d2581f40863
Sebastian <smgreenholtz@gmail.com>
2015-09-22 00:39:32 +0000

    Started project 1

---
+++ project_1/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE HTML>
+<html lang="en">
+    <head>
+        <title>Mad Libs Game!</title>
+        <link href="index.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+        
+        <?php
+            
+        
+    
+        ?>
+        
+        <form enctype="multipart/form-data" action="update_story.php" method="post" role="form">
+            <label for="noun">Enter a noun: </label>
+            <input name="noun" type="text" /> <br />
+            
+            <label for="verb">Enter a verb: </label>
+            <input name="verb" type="text" /><br />
+            
+            <label for="adjective">Enter an adjective: </label>
+            <input name="adjective" type="text" /><br />
+            
+            <label for="adverb">Enter an adverb: </label>
+            <input name="adverb" type="text" /><br />
+            
+            <input type="submit" name="Submit" />
+        </form>
+    </body>
+</html>
---
+++ project_1/update_story.php
@@ -0,0 +1,11 @@
+<?php
+
+    $noun = $_POST['noun'];
+    $adjective = $_POST['adjective'];
+    $adverb = $_POST['adverb'];
+    $verb = $_POST['verb'];
+    
+    
+    echo "Once there was a $adjective $noun from Texas that liked to " .
+        "$adverb $verb anyone passing by."
+?>
