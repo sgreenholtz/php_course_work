5646832af1cc9b30bbc8165a594ca4d73fbc383e
Sebastian <smgreenholtz@gmail.com>
2015-09-27 14:08:09 +0000

    Added all the coding from index.php to update_story.php to centralize code onto one webpage. Index.php is no longer needed

--- project_1/update_story.php
+++ project_1/update_story.php
@@ -1,26 +1,52 @@
-<form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
-    <label for="noun">Enter a noun: </label>
-    <input name="noun" type="text" /> <br />
-            
-    <label for="verb">Enter a verb (present tense): </label>
-    <input name="verb" type="text" /><br />
-            
-    <label for="adjective">Enter an adjective: </label>
-    <input name="adjective" type="text" /><br />
-            
-    <label for="adverb">Enter an adverb: </label>
-    <input name="adverb" type="text" /><br />
-            
-    <input type="submit" name="Submit" />
-</form>
-
-<?php
-
-    $noun = $_POST['noun'];
-    $adjective = $_POST['adjective'];
-    $adverb = $_POST['adverb'];
-    $verb = $_POST['verb'];
+<!DOCTYPE html>
+<html>
     
-    echo "Once there was a $adjective $noun from Texas that liked to " .
-        "$adverb $verb anyone passing by."
-?>
+    <head>
+        <title>Mad Libs Game!</title>
+        <link href="css/index.css" rel="stylesheet" />
+    </head>
+    
+    <body>
+    
+        <div class="container"> 
+            <h2>Enter words to complete the story!</h2>
+        
+            <form action="<?php $_SERVER['PHP_SELF'] ?>" method="post">
+                <label for="noun">Enter a noun: </label>
+                <input name="noun" type="text" /> <br />
+                        
+                <label for="verb">Enter a verb (present tense): </label>
+                <input name="verb" type="text" /><br />
+                        
+                <label for="adjective">Enter an adjective: </label>
+                <input name="adjective" type="text" /><br />
+                        
+                <label for="adverb">Enter an adverb: </label>
+                <input name="adverb" type="text" /><br />
+                
+                <div class="submit">  
+                    <input type="submit" name="Submit" />
+                </div>
+            </form>
+            
+            <div class="line">
+                <hr />
+            </div>
+        
+            <?php
+                if (isset($_POST['Submit']))
+                {
+                    $noun = $_POST['noun'];
+                    $adjective = $_POST['adjective'];
+                    $adverb = $_POST['adverb'];
+                    $verb = $_POST['verb'];
+                    
+                    
+                    
+                    echo "Once there was a $adjective $noun from Texas that liked to " .
+                        "$adverb $verb anyone passing by.";
+                }
+            ?>
+        </div>
+    </body>
+</html>
