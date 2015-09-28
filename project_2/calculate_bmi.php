079922699557c5d8418ea751add5ba8ad758a045
Sebastian <smgreenholtz@gmail.com>
2015-09-28 23:20:46 +0000

    working

--- project_2/calculate_bmi.php
+++ project_2/calculate_bmi.php
@@ -6,11 +6,26 @@
 
     <body>
 
-        <form method="post" action="<?php $_SERVER['PHP SELF'] ?>">
+        <form method="post" action="<?php $_SERVER['PHP_SELF'] ?>">
 
+            <label for="weight">Enter your weight: </label>
+            <input type="text" name="weight" /><br />
 
+            <label for="height">Enter your height: </label>
+            <input type="text" name="height" />
+
+            <input type="submit" name="Submit" />
 
         </form>
 
+        <?php
+
+            $weight = $_POST['weight'];
+            $height = $_POST['height'];
+
+            $bmi = ($weight / ($height × $height)) * 703;
+            echo $bmi;
+        ?>
     </body>
 </html>
+
