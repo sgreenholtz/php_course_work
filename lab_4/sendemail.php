a83619e8a59c7997f4d97ecaaab7cdf07e2039a3
Sebastian <smgreenholtz@gmail.com>
2015-09-28 22:21:58 +0000

    Body now stays sticky

--- lab_4/sendemail.php
+++ lab_4/sendemail.php
@@ -1,14 +1,14 @@
 <?php
-    if (isset($_POST['Submit'])) 
+    if (isset($_POST['Submit']))
     {
         $subject = $_POST['subject'];
         $body = $_POST['elvismail'];
         $from = 'sgreenholtz@madisoncollege.edu';
         $output_form = false;
-    
-        if (empty($subject) && empty($body)) 
+
+        if (empty($subject) && empty($body))
         {
-            echo "You did not enter a subject or email body!";  
+            echo "You did not enter a subject or email body!";
             $output_form = true;
         }
         elseif (empty($subject))
@@ -25,24 +25,24 @@
         {
             $dbc = mysqli_connect('localhost', 'sgreenholtz', '', 'elvis_store')
                 or die('Failed to connect to database.');
-        
+
             $query = "SELECT * FROM email_list";
-        
+
             $result = mysqli_query($dbc, $query);
-            
-            while ($row = mysqli_fetch_array($result)) 
+
+            while ($row = mysqli_fetch_array($result))
             {
                 $first_name = $row['first_name'];
                 $last_name = $row['last_name'];
                 $email = $row['email'];
-            
+
                 $msg = "Dear $first_name $last_name, \n$body";
-            
+
                 mail($email, $subject, $msg, 'From:' . $from);
-            
+
                 echo 'Email sent to: ' . $first_name . ' ' . $last_name . '<br />';
             } // end of while loop
-            
+
             mysqli_close($dbc);
         } // end of else send
     } // end of if isset submit
@@ -50,18 +50,18 @@
     {
         $output_form = true;
     }
-    
-    if ($output_form) 
+
+    if ($output_form)
     {
 ?>
 
     <form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
         <label for="subject">Subject of email:</label><br />
         <input id="subject" name="subject" type="text" size="30" value="<?php echo $subject; ?>" /><br />
-        
+
         <label for="elvismail">Body of email:</label><br />
-        <textarea id="elvismail" name="elvismail" rows="8" cols="40" value="<?php echo $body; ?>"></textarea><br />
-        
+        <textarea id="elvismail" name="elvismail" rows="8" cols="40"><?php echo $body; ?></textarea><br />
+
         <input type="submit" name="Submit" value="Submit" />
     </form>
 
