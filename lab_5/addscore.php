222e146564691bf5b6fd4db3b87ec5c4ab86db9b
Sebastian <smgreenholtz@gmail.com>
2015-10-03 19:42:31 +0000

    Upload works

--- lab_5/addscore.php
+++ lab_5/addscore.php
@@ -27,7 +27,7 @@ if (isset($_POST['submit']))
       if (move_uploaded_file($_FILES['screenshot']['tmp_name'], $target))
       {
         // Connect to the database
-        $dbc = mysqli_connect('DB_HOST', 'DB_USERNAME', 'DB_PW', 'DB_NAME');
+        $dbc = mysqli_connect(DB_HOST, DB_USERNAME, DB_PW, DB_NAME);
 
         // Write the data to the database
         $query = "INSERT INTO uploads VALUES (0, NOW(), '$name', '$score', '$screenshot')";
