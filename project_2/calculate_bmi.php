51f4f2e2cfbc0e2f6a4c2e5ca8c81c1f666cbf03
Sebastian <smgreenholtz@gmail.com>
2015-10-03 18:13:59 +0000

    Set up Guitar Wars webpage and database

--- project_2/calculate_bmi.php
+++ project_2/calculate_bmi.php
@@ -7,6 +7,9 @@
 
     <body>
         <div class="container">
+
+            <h1>Calculate Your Body Mass Index</h1>
+
             <form method="post" action="<?php $_SERVER['PHP_SELF'] ?>">
 
                 <label for="weight">Enter your weight (lbs): </label>
@@ -32,27 +35,43 @@
 
                     if (($weight == 0) || ($height == 0))
                     {
-                        echo "Please enter values for height and weight.";
+                ?>
+                        <div class="error">
+                            Please enter values for height and weight.
+                        </div>
+                <?php
                     }
                     else
                     {
-                        $bmi = ($weight / ($height * $height)) * 703;
-                        echo "Your BMI is " . round($bmi, 2);
+                        $bmi = round((($weight / ($height * $height)) * 703), 2);
+                    ?>
 
+                        <div class="message">
+                            You entered: <span class="entered"><?= $weight ?> lbs</span>
+                            and <span class="entered"><?= $height ?> inches</span>.
+                            <br />
+                            Your BMI is <span class="entered"><?= $bmi ?></span>.
+                        </div>
+                        <br />
+
+                    <?php
                         if ($bmi < 18.5) : ?>
                             <div class="message">
-                                You are underweight. You might want to consider
-                                consulting your doctor.
+                                You are <span class="weight">underweight</span>.
+                                <br />You might want to consider consulting your
+                                doctor.
                             </div>
                         <?php elseif ($bmi < 25) : ?>
                             <div class="message">
-                                You are at a health weight. Continue eating right
-                                and exercising regularly.
+                                You are at a <span class="weight">healthy weight
+                                </span>. Congratulations! <br />Continue eating
+                                right and exercising regularly.
                             </div>
                         <?php else : ?>
                             <div class="message">
-                                You are overweight. You might want to consider
-                                consulting your doctor.
+                                You are <span class="weight">overweight</span>.
+                                <br />You might want to consider consulting your
+                                doctor.
                             </div>
                         <?php endif;
                     }
