711efaecbcffdefb68df8f510ba971c92668010c
Sebastian <smgreenholtz@gmail.com>
2015-10-06 23:20:29 +0000

    Setting up add post page

--- project_3/add_post.php
+++ project_3/add_post.php
@@ -19,6 +19,23 @@
 <?php
 
     // form submission - add to the database
-    
+    require_once('connectvars.php');
+
+    if (isset($_POST['Submit']))
+    {
+        $dbc = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+
+        $title = $_POST['title'];
+        $entry = $_POST['blogpost'];
+        $date = date('YYYY-MM-DD');
+
+        $query = "INSERT INTO posts VALUES ('$title', '$entry', '$date')";
+        $result = mysqli_query($dbc, $query)
+            or die("Failed to upload your blog post.");
+
+    } // end of if submitted
+
+
+
     include('footer.php');
 ?>
---
+++ project_3/connectvars.php
@@ -0,0 +1,8 @@
+<?php
+
+    describe('DB_HOST', 'localhost');
+    describe('DB_USERNAME', 'sgreenholtz');
+    describe('DB_PASSWORD', '');
+    describe('DB_NAME', 'sebastian_blog');
+
+?>
