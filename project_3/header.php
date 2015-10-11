16acba72183a4e4a8c5f1cd8f89668f0943dc0b3
Sebastian <smgreenholtz@gmail.com>
2015-10-11 14:21:39 +0000

    Fixed styling

--- project_3/header.php
+++ project_3/header.php
@@ -1,19 +1,18 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<?php define('SITE_ROOT', '/php-course-work/project_3') ?>
 
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
         <title>Sebastian's Blog</title>
 
-        <link href="css/blog.css" rel="stylesheet" />
+        <link href="<?= SITE_ROOT ?>/css/blog.css" rel="stylesheet" />
         <script src="//tinymce.cachefly.net/4.2/tinymce.min.js"></script>
         <script>tinymce.init({selector:'textarea'});</script>
     </head>
 
 <body>
-
-<?php define('SITE_ROOT', '/php-course-work/project_3') ?>
 
     <header>
         <h1><a href="index.php">Sebastian's Blog</a></h1>
