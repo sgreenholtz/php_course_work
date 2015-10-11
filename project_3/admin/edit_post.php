b1720c069b6c4acfbbf0d503fd5bc26f8a297097
Sebastian <smgreenholtz@gmail.com>
2015-10-11 16:50:46 +0000

    Synchronizing changes

--- project_3/admin/edit_post.php
+++ project_3/admin/edit_post.php
@@ -4,13 +4,15 @@ require_once('../header.php');
 require_once('authenticate.php');
 require_once('../connectvars.php');
 
-if (isset($_GET['ID']) && isset($_GET['Title']) &&
-        isset($_GET['BlogPost']) && isset($_GET['DatePosted']))
+if (isset($_GET['ID']) && isset($_GET['Title']) && isset($_GET['DatePosted']))
     {
         $id = $_GET['ID'];
         $title = $_GET['Title'];
-        $blog_post = $_GET['BlogPost'];
         $date = $_GET['DatePosted'];
+
+        $get_blog_post = "SELECT BlogPost FROM posts WHERE id=$id";
+        $dbc = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
+        $blog_post = mysqli_fetch_array(mysqli_query($dbc, $get_blog_post))[0];
 ?>
 
 <div class="content">
@@ -39,7 +41,6 @@ if (isset($_GET['ID']) && isset($_GET['Title']) &&
             isset($_POST['blogpost']))
     {
         $dbc = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-
         $id = $_POST['id'];
         $title = mysqli_real_escape_string($dbc, $_POST['title']);
         $entry = mysqli_real_escape_string($dbc, $_POST['blogpost']);
