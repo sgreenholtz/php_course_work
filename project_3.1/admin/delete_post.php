ee4b1a2d14a7782e9304e9b51ad71bbd79a0f8f3
Sebastian <smgreenholtz@gmail.com>
2015-10-14 23:52:11 +0000

    Updated delete post page styles for bootstrap theme

--- project_3.1/admin/delete_post.php
+++ project_3.1/admin/delete_post.php
@@ -5,83 +5,89 @@ require_once('authenticate.php');
 require_once('../connectvars.php');
 
 if (isset($_GET['ID']) && isset($_GET['Title']) && isset($_GET['DatePosted']))
-    {
-        $id = $_GET['ID'];
-        $title = $_GET['Title'];
-        $date = $_GET['DatePosted'];
-    ?>
-    <div class="content">
-        <p>Are you sure you want to delete this post?</p>
-        <table>
-            <tr>
-                <td><strong>Title: </strong></td>
-                <td><?= $title ?></td>
-            </tr>
-            <tr>
-                <td><strong>Date: </strong></td>
-                <td><?= $date ?></td>
-            </tr>
-        </table>
+{
+    $id = $_GET['ID'];
+    $title = $_GET['Title'];
+    $date = $_GET['DatePosted'];
+?>
 
-    <?php
-    }
-    else if (isset($_POST['id']) && isset($_POST['title']) &&
-            isset($_POST['date']))
-    {
-        $id = $_POST['id'];
-        $title = $_POST['title'];
-        $date = $_POST['date'];
-    }
+    <div class="col-md-12">
+        <p><h3>Are you sure you want to delete this post?</h3></p>
+        <p>Title: <?= $title ?></p>
+        <p>Date: <?= $date ?></p>
+    </div>
+<?php
+}
+else if (isset($_POST['id']) && isset($_POST['title']) && isset($_POST['date']))
+{
+    $id = $_POST['id'];
+    $title = $_POST['title'];
+    $date = $_POST['date'];
+}
 
     if (isset($_POST['Submit']))
     {
         if ($_POST['remove'] == "yes")
         {
-            // Connect to database
             $dbc = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
-            // Query to delete the specified entry
             $query = "DELETE FROM posts WHERE ID = $id LIMIT 1";
             mysqli_query($dbc, $query)
                 or die('Failed to delete: ' . $query);
 
             mysqli_close($dbc);
+    ?>
 
-            echo "<div class='content'>";
-            echo "<p>Removed: " . $title . "<br /> Date: " . $date . "</p>";
-            echo "</div>";
+    <div class="col-md-12">
+        <p>Removed: <?= $title ?></p>
+        <p>Date: <?= $date ?></p>
+    </div>
 
-        } // end of if remove yes
-        else
-        {
-            echo "<p class='error'>Error deleting post.</p>";
+    <?php
         }
-    } // end of if submitted
+        else
+        { ?>
+            <div class="alert alert-dismissible alert-danger">
+                <button type="button" class="close" data-dismiss="alert">×</button>
+                <p>Error deleting post.</p>
+            </div>
+        <?php }
+    }
 
     else
     {
     ?>
 
-    <form method="post" action="delete_post.php"/>
-        <input type="radio" name="remove" value="yes" /> Yes
-        <input type="radio" name="remove" value="no" checked="checked" /> No
+    <div class="col-md-12">
+        <form method="post" action="delete_post.php"/>
+            <div class="radios">
+                <label>
+                    <input type="radio" name="remove" value="yes" />
+                    Yes
+                </label>
 
-        <input type="hidden" name="id" value="<?= $id ?>" />
-        <input type="hidden" name="title" value="<?= $title ?>" />
-        <input type="hidden" name="date" value="<?= $date ?>" />
+                <label>
+                    <input type="radio" name="remove" value="no" checked="checked" />
+                    No
+                </label>
+            </div>
 
-        <br />
-        <input type="Submit" value="Submit" name="Submit" />
-    </form>
+            <input type="hidden" name="id" value="<?= $id ?>" />
+            <input type="hidden" name="title" value="<?= $title ?>" />
+            <input type="hidden" name="date" value="<?= $date ?>" />
+
+            <br />
+            <input type="Submit" value="Submit" class="btn btn-danger" name="Submit" />
+        </form>
     </div>
-
     <?php
     }
     ?>
 
-    <div class="content">
-        <p><a href="delete_edit.php">&lt;&lt; Back to delete or edit page</a></p>
-    </div>
+
+    <ul class="breadcrumb">
+        <li><a href="delete_edit.php">← Back to delete or edit page</a></li>
+    </ul>
 
 <?php
 require_once('../footer.php');
