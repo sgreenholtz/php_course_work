dd5aae744401250f1da4d03779869156706dd773
Sebastian <smgreenholtz@gmail.com>
2015-10-18 15:24:16 +0000

    Setting up sign up page

---
+++ lab_7/signup.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Mismatch - Sign Up</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
+</head>
+<body>
+  <h3>Mismatch - Sign Up</h3>
+
+<?php
+    require_once('appvars.php');
+    require_once('connectvars.php');
+
+
+    mysqli_close($dbc);
+?>
+</body>
+</html>
