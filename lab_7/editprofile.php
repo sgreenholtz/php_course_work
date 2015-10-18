af307885a2b873e7360dbe522345e3fe0f52c752
Sebastian <smgreenholtz@gmail.com>
2015-10-18 14:58:47 +0000

    Updating pages to course coding standards

--- lab_7/editprofile.php
+++ lab_7/editprofile.php
@@ -4,7 +4,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>Mismatch - Edit Profile</title>
-  <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link rel="stylesheet" type="text/css" href="style/style.css" />
 </head>
 <body>
   <h3>Mismatch - Edit Profile</h3>
--- lab_7/index.php
+++ lab_7/index.php
@@ -2,44 +2,50 @@
   "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-  <title>Mismatch - Where opposites attract!</title>
-  <link rel="stylesheet" type="text/css" href="style.css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Mismatch - Where opposites attract!</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css" />
 </head>
 <body>
-  <h3>Mismatch - Where opposites attract!</h3>
+    <h3>Mismatch - Where opposites attract!</h3>
 
 <?php
-  require_once('appvars.php');
-  require_once('connectvars.php');
-
-  // Generate the navigation menu
-  echo '&#10084; <a href="viewprofile.php">View Profile</a><br />';
-  echo '&#10084; <a href="editprofile.php">Edit Profile</a><br />';
-
-  // Connect to the database 
-  $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME); 
-
-  // Retrieve the user data from MySQL
-  $query = "SELECT user_id, first_name, picture FROM mismatch_user WHERE first_name IS NOT NULL ORDER BY join_date DESC LIMIT 5";
-  $data = mysqli_query($dbc, $query);
-
-  // Loop through the array of user data, formatting it as HTML
-  echo '<h4>Latest members:</h4>';
-  echo '<table>';
-  while ($row = mysqli_fetch_array($data)) {
-    if (is_file(MM_UPLOADPATH . $row['picture']) && filesize(MM_UPLOADPATH . $row['picture']) > 0) {
-      echo '<tr><td><img src="' . MM_UPLOADPATH . $row['picture'] . '" alt="' . $row['first_name'] . '" /></td>';
-    }
-    else {
-      echo '<tr><td><img src="' . MM_UPLOADPATH . 'nopic.jpg' . '" alt="' . $row['first_name'] . '" /></td>';
-    }
-    echo '<td>' . $row['first_name'] . '</td></tr>';
-  }
-  echo '</table>';
-
-  mysqli_close($dbc);
+    require_once('appvars.php');
+    require_once('connectvars.php');
 ?>
 
-</body> 
+    &#10084; <a href="viewprofile.php">View Profile</a><br />
+    &#10084; <a href="editprofile.php">Edit Profile</a><br />
+
+<?php
+    $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
+    $query = "SELECT user_id, first_name, picture FROM mismatch_user WHERE " .
+        "first_name IS NOT NULL ORDER BY join_date DESC LIMIT 5";
+    $data = mysqli_query($dbc, $query);
+?>
+
+    <h4>Latest members:</h4>
+    <table>
+
+<?php
+    while ($row = mysqli_fetch_array($data))
+    {
+        if (is_file(MM_UPLOADPATH . $row['picture']) && filesize(MM_UPLOADPATH . $row['picture']) > 0)
+        { ?>
+            <tr><td><img src="<?= MM_UPLOADPATH . $row['picture'] ?>" alt="<?= $row['first_name'] ?>" /></td>
+        <?php }
+        else
+        { ?>
+            <tr><td><img src="<?= MM_UPLOADPATH ?>nopic.jpg" alt="<?= $row['first_name'] ?>" /></td>
+        <?php } ?>
+       <td><?= $row['first_name'] ?></td></tr>
+    <?php  }?>
+
+    </table>
+
+<?php
+    mysqli_close($dbc);
+?>
+
+</body>
 </html>
