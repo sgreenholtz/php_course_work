be26d8f989be519d1f4fac65b1bfb9f05ce1a979
Sebastian <smgreenholtz@gmail.com>
2015-10-18 15:18:06 +0000

    Adding login script

--- lab_7/viewprofile.php
+++ lab_7/viewprofile.php
@@ -4,7 +4,7 @@
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>Mismatch - View Profile</title>
-  <link rel="stylesheet" type="text/css" href="style.css" />
+  <link rel="stylesheet" type="text/css" href="style/style.css" />
 </head>
 <body>
   <h3>Mismatch - View Profile</h3>
@@ -17,68 +17,84 @@
   $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
   // Grab the profile data from the database
-  if (!isset($_GET['user_id'])) {
-    $query = "SELECT username, first_name, last_name, gender, birthdate, city, state, picture FROM mismatch_user WHERE user_id = '$user_id'";
+  if (!isset($_GET['user_id']))
+  {
+    $query = "SELECT username, first_name, last_name, gender, birthdate, city, " .
+      "state, picture FROM mismatch_user WHERE user_id = '$user_id'";
   }
-  else {
-    $query = "SELECT username, first_name, last_name, gender, birthdate, city, state, picture FROM mismatch_user WHERE user_id = '" . $_GET['user_id'] . "'";
+  else
+  {
+    $query = "SELECT username, first_name, last_name, gender, birthdate, city, " .
+      "state, picture FROM mismatch_user WHERE user_id = '" . $_GET['user_id'] . "'";
   }
   $data = mysqli_query($dbc, $query);
 
-  if (mysqli_num_rows($data) == 1) {
-    // The user row was found so display the user data
+  if (mysqli_num_rows($data) == 1)
+  {
     $row = mysqli_fetch_array($data);
     echo '<table>';
-    if (!empty($row['username'])) {
+    if (!empty($row['username']))
+    {
       echo '<tr><td class="label">Username:</td><td>' . $row['username'] . '</td></tr>';
     }
-    if (!empty($row['first_name'])) {
+    if (!empty($row['first_name']))
+    {
       echo '<tr><td class="label">First name:</td><td>' . $row['first_name'] . '</td></tr>';
     }
-    if (!empty($row['last_name'])) {
+    if (!empty($row['last_name']))
+    {
       echo '<tr><td class="label">Last name:</td><td>' . $row['last_name'] . '</td></tr>';
     }
-    if (!empty($row['gender'])) {
+    if (!empty($row['gender']))
+    {
       echo '<tr><td class="label">Gender:</td><td>';
-      if ($row['gender'] == 'M') {
+      if ($row['gender'] == 'M')
+      {
         echo 'Male';
       }
-      else if ($row['gender'] == 'F') {
+      else if ($row['gender'] == 'F')
+      {
         echo 'Female';
       }
-      else {
+      else
+      {
         echo '?';
       }
       echo '</td></tr>';
     }
-    if (!empty($row['birthdate'])) {
-      if (!isset($_GET['user_id']) || ($user_id == $_GET['user_id'])) {
-        // Show the user their own birthdate
+    if (!empty($row['birthdate']))
+    {
+      if (!isset($_GET['user_id']) || ($user_id == $_GET['user_id']))
+      {
         echo '<tr><td class="label">Birthdate:</td><td>' . $row['birthdate'] . '</td></tr>';
       }
-      else {
-        // Show only the birth year for everyone else
+      else
+      {
         list($year, $month, $day) = explode('-', $row['birthdate']);
         echo '<tr><td class="label">Year born:</td><td>' . $year . '</td></tr>';
       }
     }
-    if (!empty($row['city']) || !empty($row['state'])) {
+    if (!empty($row['city']) || !empty($row['state']))
+    {
       echo '<tr><td class="label">Location:</td><td>' . $row['city'] . ', ' . $row['state'] . '</td></tr>';
     }
-    if (!empty($row['picture'])) {
+    if (!empty($row['picture']))
+    {
       echo '<tr><td class="label">Picture:</td><td><img src="' . MM_UPLOADPATH . $row['picture'] .
         '" alt="Profile Picture" /></td></tr>';
     }
     echo '</table>';
-    if (!isset($_GET['user_id']) || ($user_id == $_GET['user_id'])) {
+    if (!isset($_GET['user_id']) || ($user_id == $_GET['user_id']))
+    {
       echo '<p>Would you like to <a href="editprofile.php">edit your profile</a>?</p>';
     }
-  } // End of check for a single row of user results
-  else {
+  }
+  else
+  {
     echo '<p class="error">There was a problem accessing your profile.</p>';
   }
 
   mysqli_close($dbc);
 ?>
-</body> 
+</body>
 </html>
