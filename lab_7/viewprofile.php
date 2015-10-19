2fae4aa663c7c8bfbb1733ca12023c7895d3475d
Sebastian <smgreenholtz@gmail.com>
2015-10-19 01:33:13 +0000

    Course standards achieved

--- lab_7/viewprofile.php
+++ lab_7/viewprofile.php
@@ -1,7 +1,6 @@
 <?php
     session_start();
 
-    // If the session vars aren't set, try to set them with a cookie
     if (!isset($_SESSION['user_id']))
     {
         if (isset($_COOKIE['user_id']) && isset($_COOKIE['username']))
@@ -28,43 +27,50 @@
     require_once('connectvars.php');
 
     if (!isset($_SESSION['user_id']))
-    {
-        echo '<p class="login">Please <a href="login.php">log in</a> to access this page.</p>';
-        exit();
+    { ?>
+        <p class="login">Please <a href="login.php">log in</a> to access this page.</p>
+        <?php exit();
     }
     else
-    {
-        echo('<p class="login">You are logged in as ' . $_SESSION['username'] . '. <a href="logout.php">Log out</a>.</p>');
-    }
+    { ?>
+        <p class="login">You are logged in as <?= $_SESSION['username'] ?>.
+        <a href="logout.php">Log out</a>.</p>
+    <?php }
 
     $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
     if (!isset($_GET['user_id']))
     {
-        $query = "SELECT username, first_name, last_name, gender, birthdate, city, state, picture FROM mismatch_user WHERE user_id = '" . $_SESSION['user_id'] . "'";
+        $query = "SELECT username, first_name, last_name, gender, birthdate, ".
+            "city, state, picture FROM mismatch_user WHERE user_id = '"
+            . $_SESSION['user_id'] . "'";
     }
     else
     {
-        $query = "SELECT username, first_name, last_name, gender, birthdate, city, state, picture FROM mismatch_user WHERE user_id = '" . $_GET['user_id'] . "'";
+        $query = "SELECT username, first_name, last_name, gender, birthdate, " .
+            "city, state, picture FROM mismatch_user WHERE user_id = '"
+            . $_GET['user_id'] . "'";
     }
     $data = mysqli_query($dbc, $query);
 
     if (mysqli_num_rows($data) == 1)
     {
-        // The user row was found so display the user data
         $row = mysqli_fetch_array($data);
         echo '<table>';
         if (!empty($row['username']))
         {
-            echo '<tr><td class="label">Username:</td><td>' . $row['username'] . '</td></tr>';
+            echo '<tr><td class="label">Username:</td><td>' .
+                $row['username'] . '</td></tr>';
         }
         if (!empty($row['first_name']))
         {
-            echo '<tr><td class="label">First name:</td><td>' . $row['first_name'] . '</td></tr>';
+            echo '<tr><td class="label">First name:</td><td>' .
+                $row['first_name'] . '</td></tr>';
         }
         if (!empty($row['last_name']))
         {
-            echo '<tr><td class="label">Last name:</td><td>' . $row['last_name'] . '</td></tr>';
+            echo '<tr><td class="label">Last name:</td><td>' .
+                $row['last_name'] . '</td></tr>';
         }
         if (!empty($row['gender']))
         {
@@ -87,27 +93,32 @@
         {
             if (!isset($_GET['user_id']) || ($_SESSION['user_id'] == $_GET['user_id']))
             {
-                echo '<tr><td class="label">Birthdate:</td><td>' . $row['birthdate'] . '</td></tr>';
+                echo '<tr><td class="label">Birthdate:</td><td>' .
+                    $row['birthdate'] . '</td></tr>';
             }
             else
             {
                 list($year, $month, $day) = explode('-', $row['birthdate']);
-                echo '<tr><td class="label">Year born:</td><td>' . $year . '</td></tr>';
+                echo '<tr><td class="label">Year born:</td><td>' .
+                    $year . '</td></tr>';
             }
         }
         if (!empty($row['city']) || !empty($row['state']))
         {
-            echo '<tr><td class="label">Location:</td><td>' . $row['city'] . ', ' . $row['state'] . '</td></tr>';
+            echo '<tr><td class="label">Location:</td><td>' .
+                $row['city'] . ', ' . $row['state'] . '</td></tr>';
         }
         if (!empty($row['picture']))
         {
-            echo '<tr><td class="label">Picture:</td><td><img src="' . MM_UPLOADPATH . $row['picture'] .
+            echo '<tr><td class="label">Picture:</td><td><img src="' .
+                MM_UPLOADPATH . $row['picture'] .
                 '" alt="Profile Picture" /></td></tr>';
         }
         echo '</table>';
         if (!isset($_GET['user_id']) || ($_SESSION['user_id'] == $_GET['user_id']))
         {
-            echo '<p>Would you like to <a href="editprofile.php">edit your profile</a>?</p>';
+            echo '<p>Would you like to <a href="editprofile.php">' .
+                'edit your profile</a>?</p>';
         }
     }
     else
