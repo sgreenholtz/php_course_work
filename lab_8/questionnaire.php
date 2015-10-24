642aa00ed467d96d0ed7c1c0a3c1d29aef1fee76
Sebastian <smgreenholtz@gmail.com>
2015-10-24 18:03:39 +0000

    Fixed SQL errors

--- lab_8/questionnaire.php
+++ lab_8/questionnaire.php
@@ -14,8 +14,8 @@
 
     $dbc = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME);
 
-    $select_response_query = "SELECT * FROM mismatch_reponse WEHRE user_id = '" .
-        $_SESSION['user_id'] . "'";
+    $select_response_query = "SELECT * FROM mismatch_response " .
+        "WHERE user_id = '" . $_SESSION['user_id'] . "'";
     $response_data = mysqli_query($dbc, $select_response_query);
 
     if (mysqli_num_rows($response_data) == 0)
@@ -36,15 +36,14 @@
                 "(user_id, topic_id) VALUES (" . $_SESSION['user_id'] . ", " .
                 "$topic_id)";
 
-            mysqli_query($dbc, $insert_blanks_query) or
-                die("Error inserting answers");
+            mysqli_query($dbc, $insert_blanks_query);
 
         }
     }
 
     if (isset($_POST['Submit']))
     {
-        foreach($_POST as $response_id)
+        foreach($_POST as $response_id => $response)
         {
             $insert_answers_query = "UPDATE mismatch_reponse SET response = " .
                 "$response WHERE response_id = $response_id";
@@ -56,29 +55,29 @@
     <?php
     }
 
-$create_form_query = "SELECT response_id, topic_id, response " .
-    "FROM mismatch_reponse WEHRE user_id = '" . $_SESSION['user_id'] . "'";
+    $create_form_query = "SELECT response_id, topic_id, response " .
+        "FROM mismatch_response WHERE user_id = '" . $_SESSION['user_id'] . "'";
 
-$data_for_form = mysqli_query($dbc, $create_form_query);
-$responses = array();
+    $data_for_form = mysqli_query($dbc, $create_form_query);
+    $responses = array();
 
-while ($select_row = mysqli_fetch_array($data_for_form))
-{
-    $topic_query = "SELECT name, category FROM mismatch_topic " .
-        "WHERE topic_id = '" . $row['topic_id'] . "'";
-    $topic_data = mysqli_query($dbc, $topic_query);
-
-    if (mysqli_num_rows($topic_data) == 1)
+    while ($select_row = mysqli_fetch_array($data_for_form))
     {
-        $topic_row = mysqli_fetch_array($topic_data);
+        $topic_query = "SELECT name, category_id FROM mismatch_topic " .
+            "WHERE topic_id = '" . $select_row['topic_id'] . "'";
+        $topic_data = mysqli_query($dbc, $topic_query);
 
-        $select_row['topic_name'] = $topic_row['name'];
-        $select_row['category_name'] = $topic_row['category'];
-        array_push($responses, $select_row);
+        if (mysqli_num_rows($topic_data) == 1)
+        {
+            $topic_row = mysqli_fetch_array($topic_data);
+
+            $select_row['topic_name'] = $topic_row['name'];
+            $select_row['category_name'] = $topic_row['category'];
+            array_push($responses, $select_row);
+        }
     }
-}
 
-mysqli_close();
+    mysqli_close($dbc);
 ?>
 
 <form method="post" action="<?= $_SERVER['PHP_SELF'] ?>" />
@@ -117,7 +116,7 @@ mysqli_close();
     ?>
 
     </fieldset>
-    <input type="submit" value="Save Questionnaire" name="Submit" />;
+    <input type="submit" value="Save Questionnaire" name="Submit" />
 </form>
 
 
