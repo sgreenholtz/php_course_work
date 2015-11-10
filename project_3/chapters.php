c544f9c75e1dfeb374b856473ebbe64ae99aeb9c
Sebastian <smgreenholtz@gmail.com>
2015-11-10 00:19:34 +0000

    Working on chapters output page

--- project_3/chapters.php
+++ project_3/chapters.php
@@ -3,7 +3,7 @@
     require_once('connectvars.php');
 
     $dbc = mysqli_connect(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
-    $query = "SELECT * FROM posts ORDER BY id DESC;";
+    $query = "SELECT * FROM chapters ORDER BY id ;";
     $result = mysqli_query($dbc, $query);
     if (!$result)
     {
@@ -11,17 +11,15 @@
     }
 ?>
 <div class="container">
-    <?php while ($record = mysqli_fetch_assoc($result))
-    { ?>
+    <?php while ($record = mysqli_fetch_assoc($result)) { ?>
     <div class="col-md-12">
         <div class="well">
-            <h3><?= $record['Title'] ?></h3>
-            <p><?= $record['BlogPost'] ?></p>
+            <h3><?= $record['ID'] ?></h3>
+            <p><?= $record['ChapterText'] ?></p>
             <p class="label label-info">Posted: <?= $record['DatePosted'] ?></p>
         </div>
     </div>
     <?php } ?>
 </div>
-<?php
-    include('footer.php');
-?>
+
+<?php include('footer.php'); ?>
