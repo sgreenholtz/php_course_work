9e3a2ccb5b354b014bf781af0028c06d94bcaf39
Sebastian <smgreenholtz@gmail.com>
2015-11-10 00:16:07 +0000

    Creating Chapters table

--- project_3/sebastian_blog.sql
+++ project_3/sebastian_blog.sql
@@ -1,7 +1,16 @@
-Create table posts (
+USE sebastian_blog;
+
+CREATE TABLE IF NOT EXISTS posts (
     ID INT(11) NOT NULL AUTO_INCREMENT,
     Title VARCHAR(255) NOT NULL,
     BlogPost TEXT,
+    DatePosted DATE,
+    PRIMARY KEY (ID)
+);
+
+CREATE TABLE IF NOT EXISTS chapters (
+    ID INT(11) NOT NULL AUTO_INCREMENT;
+    ChapterText TEXT,
     DatePosted DATE,
     PRIMARY KEY (ID)
 );
