c0f6614ace33eee4fa333be2b9f464cf6f5f94b5
Sebastian <smgreenholtz@gmail.com>
2015-11-22 17:49:49 +0000

    Forgot to add the ! to mean does NOT match

--- lab_11/test_regex.php
+++ lab_11/test_regex.php
@@ -1,8 +1,10 @@
 <?php
-$phone_regex = '/^\(?[^0-1]\d{2}\)?(-|\s)?\d{3}(-|\s)?\d{4}$/';
+$phone_regex = "/^\(?[2-9]\d{2}\)?(\-|\s)?\d{3}(\-|\s)?\d{4}$/";
+$phone_replace = "/[\(\)\-\s]/";
 
-if (preg_match($phone_regex, $phone)) : ?>
+if (!(preg_match($phone_regex, $phone))) : ?>
     <p class="error">Your phone number is entered incorrectedly.</p>
+    <p><?= $phone ?></p>
 <?php
     $output_form = 'yes';
 else :
