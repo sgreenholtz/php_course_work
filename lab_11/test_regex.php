32eb43f9556cc2d088620874a94b00d60c1b43e7
Sebastian <smgreenholtz@gmail.com>
2015-11-22 17:54:58 +0000

    Added regex validation for email

--- lab_11/test_regex.php
+++ lab_11/test_regex.php
@@ -2,13 +2,20 @@
 $phone_regex = "/^\(?[2-9]\d{2}\)?(\-|\s)?\d{3}(\-|\s)?\d{4}$/";
 $phone_replace = "/[\(\)\-\s]/";
 
+$email_regex = "/^.+@.+\..+$/";
+
 if (!(preg_match($phone_regex, $phone))) : ?>
     <p class="error">Your phone number is entered incorrectedly.</p>
-    <p><?= $phone ?></p>
 <?php
     $output_form = 'yes';
 else :
-    $phone = preg_replace($phone_replace_regex, '', $phone);
+    $phone = preg_replace($phone_replace, '', $phone);
+endif;
+
+if (!(preg_match($email_regex, $email))) : ?>
+    <p class="error">Your email is entered incorrectly.</p>
+<?php
+    $output_form = 'yes';
 endif;
 
 ?>
