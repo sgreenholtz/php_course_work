f3490bef68a5814fd42e6eaaf65406f9b534dca7
Sebastian <smgreenholtz@gmail.com>
2015-11-29 15:51:53 +0000

    Created captcha file

--- lab_12/captcha.php
+++ lab_12/captcha.php
@@ -1,8 +1,38 @@
 <?php
-    $passphrase = "";
-    for ($i=0; $i<7; $i++)
-    {
-        $passphrase .= chr(rand(97, 122));
-    }
+
+define('CAPTCHA_WIDTH', 100);
+define('CAPTCHA_HEIGHT', 25);
+
+$img = imagecreatetruecolor(CAPTCHA_WIDTH, CAPTCHA_HEIGHT);
+$bg_color = imagecolorallocate($img, 225, 255, 255);
+$text_color = imagecolorallocate($img, 0, 0, 0);
+$graphic_color = imagecolorallocate($img, 64, 64, 64);
+
+$passphrase = "";
+for ($i=0; $i<7; $i++)
+{
+    $passphrase .= chr(rand(97, 122));
+}
+
+imagefilledrectangle($img, 0, 0, CAPTCHA_WIDTH, CAPTCHA_HEIGHT, $bg_color);
+
+for ($i=0; $i<5; $i++)
+{
+    imageline($img, 0, rand() % CAPTCHA_HEIGHT, CAPTCHA_WIDTH,
+        rand() % CAPTCHA_HEIGHT, $graphic_color);
+}
+
+for ($i=0; $i<50; $i++)
+{
+    imagesetpixel($img, rand() % CAPTCHA_HEIGHT, CAPTCHA_WIDTH,
+        rand() % CAPTCHA_HEIGHT, $graphic_color);
+}
+
+imagettftext($img, 18, 0, 5, CAPTCHA_HEIGHT-5, $text_color,
+    'Courier New Bold.ttf', $passphrase);
+
+header("Content-type: image/png");
+imagepng($img);
+
 
 ?>
