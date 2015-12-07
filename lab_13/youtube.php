3c31231cf13395d67caecd63d57b9a2b665af7ea
Sebastian <smgreenholtz@gmail.com>
2015-12-07 03:19:36 +0000

    Deleted Composer and Google packages, added a workaround for xml for youtube

--- lab_13/youtube.php
+++ lab_13/youtube.php
@@ -1,92 +1,51 @@
 <?php
-require_once('../vendor/autoload.php');
-$htmlBody = <<<END
-<form method="GET">
-  <div>
-    Search Term: <input type="search" id="q" name="q" placeholder="Enter Search Term">
-  </div>
-  <div>
-    Max Results: <input type="number" id="maxResults" name="maxResults" min="1" max="50" step="1" value="25">
-  </div>
-  <input type="submit" value="Search">
-</form>
-END;
+    define('YOUTUBE_URL', 'https://www.youtube.com/feeds/videos.xml?user=aliensabductedme');
+    define('NUM_VIDEOS', 5);
 
-// This code will execute if the user entered a search query in the form
-// and submitted the form. Otherwise, the page displays the form above.
-if ($_GET['q'] && $_GET['maxResults']) {
-  // Call set_include_path() as needed to point to your client library.
-require_once 'Google/Client.php';
-require_once 'Google/Service/YouTube.php';
+    $xml = simplexml_load_file(YOUTUBE_URL);
 
-  /*
-   * Set $DEVELOPER_KEY to the "API key" value from the "Access" tab of the
-   * Google Developers Console <https://console.developers.google.com/>
-   * Please ensure that you have enabled the YouTube Data API for your project.
-   */
-  $DEVELOPER_KEY = 'AIzaSyAEWHsonh1L8oD6iVwHedYFrIDjo7BuL84';
+    $num_videos_found = count($xml->entry);
 
-  $client = new Google_Client();
-  $client->setDeveloperKey($DEVELOPER_KEY);
+    if ($num_videos_found > 0)
+    {
+    ?>
+        <table><tr>
+    <?php
+    for ($i = 0; $i < min($num_videos_found, NUM_VIDEOS); $i++)
+    {
+        $entry = $xml->entry[$i];
+        $media = $entry->children('http://search.yahoo.com/mrss/');
+        $title = $media->group->title;
 
-  // Define an object that will be used to make all API requests.
-  $youtube = new Google_Service_YouTube($client);
+        $yt = $media->children('http://gdata.youtube.com/schemas/2007');
+        $attrs = $yt->duration->attributes();
+        $length_min = floor($attrs['seconds'] / 60);
+        $length_sec = $attrs['seconds'] % 60;
+        $length_formatted = $length_min . (($length_min != 1) ? ' minutes, ':' minute, ') .
+        $length_sec . (($length_sec != 1) ? ' seconds':' second');
 
-  try {
-    // Call the search.list method to retrieve results matching the specified
-    // query term.
-    $searchResponse = $youtube->search->listSearch('id,snippet', array(
-      'q' => $_GET['q'],
-      'maxResults' => $_GET['maxResults'],
-    ));
+        $attrs = $media->group->player->attributes();
+        $video_url = $attrs['url'];
 
-    $videos = '';
-    $channels = '';
-    $playlists = '';
+        $attrs = $media->group->thumbnail[0]->attributes();
+        $thumbnail_url = $attrs['url'];
 
-    // Add each result to the appropriate list, and then display the lists of
-    // matching videos, channels, and playlists.
-    foreach ($searchResponse['items'] as $searchResult) {
-      switch ($searchResult['id']['kind']) {
-        case 'youtube#video':
-          $videos .= sprintf('<li>%s (%s)</li>',
-              $searchResult['snippet']['title'], $searchResult['id']['videoId']);
-          break;
-        case 'youtube#channel':
-          $channels .= sprintf('<li>%s (%s)</li>',
-              $searchResult['snippet']['title'], $searchResult['id']['channelId']);
-          break;
-        case 'youtube#playlist':
-          $playlists .= sprintf('<li>%s (%s)</li>',
-              $searchResult['snippet']['title'], $searchResult['id']['playlistId']);
-          break;
-      }
+        ?>
+        <td style="vertical-align:bottom; text-align:center" width="
+            <?= (100 / NUM_VIDEOS) ?>%"><a href="<?= $video_url ?>">
+            <?= $title ?><br /><span style="font-size:smaller">
+            <?= $length_formatted ?></span><br /><img src="
+            <?= $thumbnail_url ?>" /></a></td>
+        <?php
+        }
+        ?>
+    </tr></table>
+    <?php
     }
-
-    $htmlBody .= <<<END
-    <h3>Videos</h3>
-    <ul>$videos</ul>
-    <h3>Channels</h3>
-    <ul>$channels</ul>
-    <h3>Playlists</h3>
-    <ul>$playlists</ul>
-END;
-  } catch (Google_Service_Exception $e) {
-    $htmlBody .= sprintf('<p>A service error occurred: <code>%s</code></p>',
-      htmlspecialchars($e->getMessage()));
-  } catch (Google_Exception $e) {
-    $htmlBody .= sprintf('<p>An client error occurred: <code>%s</code></p>',
-      htmlspecialchars($e->getMessage()));
-  }
-}
+    else
+    {
+    ?>
+    <p>Sorry, no videos were found.</p>
+<?php
+    }
 ?>
-
-<!doctype html>
-<html>
-    <head>
-      <title>YouTube Search</title>
-    </head>
-    <body>
-      <?=$htmlBody?>
-    </body>
-</html>
